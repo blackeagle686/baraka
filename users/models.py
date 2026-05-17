1b0731e02fc7781ab4c84bbf08de8ec1c521279f
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-17 08:34:16 +0300

    Remove username field and set phone as USERNAME_FIELD

--- users/models.py
+++ users/models.py
@@ -8,9 +8,19 @@ class UserRole(models.TextChoices):
     ADMIN = 'ADMIN', 'Admin'
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    username = None # Remove username field
+    phone = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to='profiles/', blank=True, null=True)
     role = models.CharField(
         max_length=20,
         choices=UserRole.choices,
         default=UserRole.CUSTOMER
     )
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone

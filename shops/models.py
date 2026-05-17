49f2604c54e56a7e60408ee9c15ccaed2b8a5d89
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-17 17:24:46 +0300

    Added quantity field to Product model

--- shops/models.py
+++ shops/models.py
@@ -31,6 +31,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=10)
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 

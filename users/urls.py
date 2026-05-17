41b326c10fc5a70ce62e44b967cbd111e364e16d
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-17 18:25:47 +0300

    Added admin user and stats endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView
+from .views import RegisterView, ProfileView, AdminUserListView, AdminUserDetailView, AdminStatsView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
+
+    # Admin endpoints
+    path('admin/users/', AdminUserListView.as_view(), name='admin-user-list'),
+    path('admin/users/<int:pk>/', AdminUserDetailView.as_view(), name='admin-user-detail'),
+    path('admin/stats/', AdminStatsView.as_view(), name='admin-stats'),
 ]

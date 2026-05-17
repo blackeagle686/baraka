8b4418c81e029636807233d8813f1f9b0e4741d1
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-17 14:06:54 +0300

    Added delivery_price field to Order model

--- orders/models.py
+++ orders/models.py
@@ -24,6 +24,7 @@ class Order(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='shop_orders')
     driver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='driver_orders')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     status = models.CharField(max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING)
     address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

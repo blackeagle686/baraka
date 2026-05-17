e15e99f636295ef0b3d4a1a5647fa77e31bd2cd9
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-17 18:26:08 +0300

    Added admin order list view to URL patterns

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
+from .views import OrderViewSet, AdminOrderListView
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/orders/', AdminOrderListView.as_view(), name='admin-order-list'),
 ]
+

e63294ca200094ea8ac3cf9068ea352ca080fcae
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-19 10:53:02 +0300

    Removed unused CLIENT_CLASS from CACHES setting

--- backend/settings.py
+++ backend/settings.py
@@ -126,7 +126,6 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
         'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'CONNECTION_POOL_KWARGS': {
                 'max_connections': 100,
                 'retry_on_timeout': True,

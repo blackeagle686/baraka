bf02f9cbe4051f7e5e0f3f2aac90f93486412a60
blackeagle686 <mathematecs1@gmail.com>
2026-05-22 17:45:10 +0300

    Removed unused cache invalidation signals from models.py

--- clinics/models.py
+++ clinics/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-from django.core.cache import cache
 
 
 class Clinic(models.Model):
@@ -149,19 +146,4 @@ class ClinicNotification(models.Model):
         return f"{self.user.phone} - {self.title}"
 
 
-@receiver([post_save, post_delete], sender=Clinic)
-def invalidate_clinics_cache(sender, instance, **kwargs):
-    try:
-        current_version = cache.get("clinics_list_version", 1)
-        cache.set("clinics_list_version", current_version + 1, timeout=None)
-    except Exception:
-        pass
 
-
-@receiver([post_save, post_delete], sender=MedicalService)
-def invalidate_services_cache(sender, instance, **kwargs):
-    try:
-        current_version = cache.get("services_list_version", 1)
-        cache.set("services_list_version", current_version + 1, timeout=None)
-    except Exception:
-        pass

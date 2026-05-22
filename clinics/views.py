8e1846375dedab109ed12345ed5f7d795ae75808
blackeagle686 <mathematecs1@gmail.com>
2026-05-22 18:26:07 +0300

    Added import for models

--- clinics/views.py
+++ clinics/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from django.core.cache import cache
+from django.db import models
 from django.shortcuts import get_object_or_404
 
 import hashlib
